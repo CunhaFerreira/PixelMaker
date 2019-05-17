646b5d362550d61acfbebd184a4ebd30510f981d
Paola <47130293+CunhaFerreira@users.noreply.github.com>
2019-05-17 14:32:34 +1200

    Update designs.js

--- js/designs.js
+++ js/designs.js
@@ -1,18 +1,34 @@
 /*jshint esversion: 6 */
-
 const table = $('#pixel_canvas');
 const inputHeight = $('#input_height');
 const inputWidth = $('#input_width');
 
-
+/*
+ *** Make Grid ***
+ */
 function makeGrid(x) {
 
-  /*Select size input*/
+  /* Select size input */
   const height = parseInt(inputHeight.val());
   const width = parseInt(inputWidth.val());
 
+  /* Shows a message if number is greater than 50 */
+  if (inputHeight.val() > 50) {
+
+    const message = "Valid number less than 50";
+    return alert(message);
+
+  }
+  if (inputWidth.val() > 50) {
+
+    const message = "Valid number less than 50";
+    return alert(message);
+
+  }
+
   table.children().remove();
 
+  /* Create the Grid */
   for (let row = 0; row < height; row++) {
 
     const tr = $('<tr></tr>');
@@ -22,18 +38,40 @@ function makeGrid(x) {
     }
     table.append(tr);
   }
+
 }
+
 $('#submitBtn').on('click', makeGrid);
 
-/* Painting */
-$('table').on('mousedown', 'td', function() {
+/*
+ *** Painting ***
+ */
+$('table').on('mousedown', 'td', function () {
   var color;
   color = $('#colorPicker').val();
   $(this).attr("bgcolor", color);
 
   /* Remove color doubleClick*/
-  $(this).dblclick(function() {
+  $(this).dblclick(function () {
     $(this).removeAttr('bgcolor');
   });
 
 });
+
+/*
+ *** Make a Default Grid ***
+ */
+function def() {
+  for (var row = 0; row < 10; row += 1) {
+    var para = document.createElement("tr");
+
+    for (var col = 0; col < 10; col += 1) {
+      var node = document.createElement("td");
+      para.appendChild(node);
+      var element = document.getElementById("pixel_canvas");
+      element.appendChild(para);
+
+    }
+  }
+}
+def();
